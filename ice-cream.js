3e07894d2e36f7b800f3f9ef80d715b0fdaad642
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-10-25 19:00:06 +0000

    Tinkering with Ice cream Arrays exercise

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
