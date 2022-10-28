86309560795cef28ac92b67d77c694fdc71c959a
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-10-28 03:03:23 +0000

    carPassing function, creates new car object

--- tinkering.js
+++ tinkering.js
@@ -1,14 +1,8 @@
-function smartGarbage(trash, bins) {
-  if(trash === "waste")
-    bins.waste++;
-  else if(trash === "recycling")
-    bins.recycling++;
-  else if(trash === "compost") 
-    bins.compost++;
-  else
-    return "Improper trash type";
-
-  return bins;
+const carPassing = function(cars, speed) {
+  const car = {
+    time: Date.now(),
+    speed: speed
+  };
+  cars.push(car);
+  return cars;
 }
-
-console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
