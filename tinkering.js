576b12d86e544eb6863ca2364f5f20f33cb38c45
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-10-28 02:52:00 +0000

    smartGarbage function returning objects

--- tinkering.js
+++ tinkering.js
@@ -1,16 +1,14 @@
-function concat(array1, array2) {
-  let newArray = [];
-  for (let i = 0; i < array1.length; i++) {
-    newArray.push(array1[i]);
-  }
-  for (let j = 0; j < array2.length; j++) {
-    newArray.push(array2[j]);
-  }
+function smartGarbage(trash, bins) {
+  if(trash === "waste")
+    bins.waste++;
+  else if(trash === "recycling")
+    bins.recycling++;
+  else if(trash === "compost") 
+    bins.compost++;
+  else
+    return "Improper trash type";
 
-  return newArray;
+  return bins;
 }
 
-console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
-console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
-console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
-console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
