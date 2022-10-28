a2b9920d1c407fe8303e976693e8c32fcd9edca2
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-10-28 16:01:33 +0000

    Object - We're Rooting for You! works in VS but not in Compass

--- tinkering.js
+++ tinkering.js
@@ -1,8 +1,42 @@
-const carPassing = function(cars, speed) {
-  const car = {
-    time: Date.now(),
-    speed: speed
-  };
-  cars.push(car);
-  return cars;
+const judgeVegetable = function(vegetables, metric) {
+  let indexOfWinner = 0;
+  switch(metric) {
+  case "redness":
+    for(let i = 1; i < vegetables.length; i++) {
+      if(vegetables[i].redness > vegetables[indexOfWinner].redness)
+        indexOfWinner = i;
+    }
+    break;
+  case "plumpness":
+    for(let i = 1; i < vegetables.length; i++) {
+      if(vegetables[i].plumpness > vegetables[indexOfWinner].plumpness)
+        indexOfWinner = i;
+    }
+    break;
+  default:
+    return "Invalid metric";
+  }
+  return vegetables[indexOfWinner].submitter;
 }
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+console.log(judgeVegetable(vegetables, metric));
