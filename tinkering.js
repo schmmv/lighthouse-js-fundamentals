6a08c2b2a8bd929224153d082f1fad0d5becb8fc
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-04 21:07:20 +0000

    stretch loopyLighthouse function

--- tinkering.js
+++ tinkering.js
@@ -1,37 +1,16 @@
-const judgeVegetable = function(vegetables, metric) {
-  let indexOfWinner = 0;
-  for(let i = 1; i < vegetables.length; i++) {
-
-    if (metric === "plumpness") {
-      if (vegetables[i].plumpness > vegetables[indexOfWinner].plumpness)
-        indexOfWinner = i;
-    } else if (metric === "redness") {
-      if (vegetables[i].redness > vegetables[indexOfWinner].redness)
-        indexOfWinner = i;
+function loopyLighthouse(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0) {
+      if (i % multiples[1] === 0)
+        console.log(words[0] + words[1]);
+      else
+        console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+        console.log(words[1]);
+    } else {
+        console.log(i);
     }
   }
-    return vegetables[indexOfWinner].submitter;
-  
 };
 
-const vegetables = [
-  {
-    submitter: 'Old Man Franklin',
-    redness: 10,
-    plumpness: 5
-  },
-  {
-    submitter: 'Sally Tomato-Grower',
-    redness: 2,
-    plumpness: 8
-  },
-  {
-    submitter: 'Hamid Hamidson',
-    redness: 4,
-    plumpness: 3
-  }
-]
-
-const metric = 'redness';
-
-console.log(judgeVegetable(vegetables, metric));
+loopyLighthouse([15,90], [2,5], ["Batty", "Beacon"]);
